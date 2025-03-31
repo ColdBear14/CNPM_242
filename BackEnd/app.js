e7c07d38fa9359897e0bc75011b7807feb428e4d
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2025-03-31 11:37:41 +0700

    init

---
+++ BackEnd/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+const bookingRoutes = require('./routes/booking');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/view/index.html');
+// });
+
+app.use('/public', express.static('public'));
+app.use('/api', bookingRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

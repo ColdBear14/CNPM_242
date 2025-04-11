e93fdd5524e46e06c9ba2856f69e2233c3298b66
Huy-Egoist <huy.luuegoist@hcmut.edu.vn>
2025-04-11 13:41:33 +0700

    no message

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,13 +14,8 @@ const cors = require('cors');
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use('/public', express.static('public'));
-<<<<<<< HEAD
-app.use('/api/auth', authRoutes);
-app.use('/api', bookingRoutes);
-=======
 app.use('/api/booking', bookingRoutes);
 app.use('/api/auth', authRoutes);
->>>>>>> b58c626e6ac741f61dacbccf5b6b15df399dcf02
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

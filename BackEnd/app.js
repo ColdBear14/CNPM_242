1efbb2ce1d7d86f477d932b19a4dc3e7aae28f01
Huy-Egoist <huy.luuegoist@hcmut.edu.vn>
2025-04-11 13:20:44 +0700

    no message

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const authRoutes = require('./routes/authRoutes')
 const bookingRoutes = require('./routes/booking');
 
 app.use(express.json());
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/public', express.static('public'));
+app.use('/api/auth', authRoutes);
 app.use('/api', bookingRoutes);
 
 app.listen(port, () => {
